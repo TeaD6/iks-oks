dbb0d5456d23dc2155d9fa0c2cb5ceb709fa74cb
TeaD6 <tea.dugandzic@gmail.com>
2023-06-28 21:46:45 +0100

    Modified and added table print in iksoks.py#

--- iksoks.py
+++ iksoks.py
@@ -1,3 +1,17 @@
 #!/bin/python3
 
 print("Ovo je nasa verzija XO igrice")
+
+tabela=[ "tea",  1,2,3, [1,2,3],    [4,5,6],    [7,8,9]]
+
+#tabela=["para'dajs","paprika","kupus"]
+
+
+print(tabela)
+print()
+a=1
+print(a)
+a=2
+print(a)
+for a in tabela:
+    print(a)
